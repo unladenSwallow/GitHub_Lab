de3fa5c2d9d3781c86fedd267569ffd82b311092
unladenSwallow <pedrol@uw.edu>
2016-10-12 17:45:08 -0700

    HELLO!

---
+++ app/src/main/java/com/example/laeghna/github_lab/LeslieActivity.java
@@ -0,0 +1,13 @@
+package com.example.laeghna.github_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeslieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leslie);
+    }
+}
